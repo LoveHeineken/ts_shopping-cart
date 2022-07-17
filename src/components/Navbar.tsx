4a7cfeb8899fb0feaba4b7ab009112a2c5e951c7
LoveHeineken <ikeno.taroh@gmail.com>
2022-07-17 17:07:11 +0900

    second commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,22 +3,22 @@ import { NavLink } from "react-router-dom"
 import { useShoppingCart } from "../context/ShoppingCartContext"
 
 export function Navbar() {
-    const { openCart, cartQuantity } = useShoppingCart()
-    return (
+  const { openCart, cartQuantity } = useShoppingCart()
+  return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
-        <Container className="bg-white shadow-sm mb-3">
-            <Nav className="me-auto">
-            <Nav.Link to="/" as={NavLink}>
-                    Home
-                </Nav.Link>
-                <Nav.Link to="/store" as={NavLink}>
-                    Store
-                </Nav.Link>
-                <Nav.Link to="/about" as={NavLink}>
-                    About
-                </Nav.Link>
-            </Nav>
-            {cartQuantity > 0 && (
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        {cartQuantity > 0 && (
           <Button
             onClick={openCart}
             style={{ width: "3rem", height: "3rem", position: "relative" }}
@@ -49,7 +49,7 @@ export function Navbar() {
             </div>
           </Button>
         )}
-        </Container>
+      </Container>
     </NavbarBs>
-    )
+  )
 }
